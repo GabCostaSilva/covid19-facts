88e01b80912facb759f6eb1256de9539f0df2ab1
Gabriel Costa <gabcostadeveloper@gmail.com>
2020-09-30 20:33:42 -0400

    genesis

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Pub Data Consumer">
+      </header>
+    </div>
+  );
+}
+
+export default App;
