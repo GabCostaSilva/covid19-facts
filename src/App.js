8ff9e797896190e86610b56950c9943e6d588ce7
Gabriel Costa <gabcostadeveloper@gmail.com>
2020-10-05 19:29:48 -0400

    deixa tabela bonitinha

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import getData from "./api"
 import "./App.css";
-// "cases": 310526,
-// "deaths": 6744,
-// "suspects": 573,
+
 function App(props) {
   const [apiData, setApiData] = useState([])
   useEffect(() => {
@@ -14,16 +12,16 @@ function App(props) {
   }, []);
   return (
     <div className="wrapper">
-      <header className="title">
+      <header className="title mb-5">
         <h1>{props.title}</h1>
       </header>
-      <article>
+      <article className="mb-4">
         <h2>O que é este projeto</h2>
         <p>Um sistema de divulgação de dados do COVID-19 nos estados do Brasil.</p>
       </article>
       <article>
         <div className="grid">
-          <div className="row">
+          <div className="row headings pb-2">
             <div className="col">
               <h4>Estado</h4>
             </div><div className="col">
@@ -34,18 +32,22 @@ function App(props) {
               <h4>Suspeitos</h4>
             </div>
           </div>
-          {apiData.map((dataReceived, key) => (
-            <div className="row" key={key}>
-              <div className="col">
+          {apiData.length ? apiData.map((dataReceived, key) => (
+            <div className="row data-rows align-items-center pb-3" key={key}>
+              <div className="col align-middle">
                 {dataReceived.uf}
               </div>
-              <div className="col">{dataReceived.cases}</div>
-              <div className="col">{dataReceived.deaths}</div>
-              <div className="col">{dataReceived.suspects}</div>
-            </div>))}
+              <div className="col align-middle">{dataReceived.cases}</div>
+              <div className="col align-middle">{dataReceived.deaths}</div>
+              <div className="col align-middle">{dataReceived.suspects}</div>
+            </div>)) : <div style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center"
+            }}>carregando...</div>}
         </div>
       </article>
-    </div>
+    </div >
   );
 }
 
