c0779454405c878811de8d06e26a4f69a7596466
guilhermercarvalho <guilhermercarvalho512@gmail.com>
2020-10-06 16:27:13 +0000

    Add: API route to check availability

--- src/api.js
+++ src/api.js
@@ -1,7 +1,28 @@
-const apiUrl = "https://covid19-brazil-api.now.sh/api/report/v1"
+const apiUrlBase = "https://covid19-brazil-api.now.sh/api/report/v1/"
+const apiUrlStatus = "https://covid19-brazil-api.now.sh/api/status/v1"
+
+/**
+ * URLs
+ * busca por estado -> 'https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/sp'
+ * busca por data   -> 'https://covid19-brazil-api.now.sh/api/report/v1/brazil/20200318'
+ * busca por país   -> 'https://covid19-brazil-api.now.sh/api/report/v1/countries'
+ */
+
+/**
+ * TODO
+ * 
+ * - implementar inputs para realizar:
+ *  . busca por estados
+ *      .. deve-se listar todos os estados disponíveis
+ *  . busca por países
+ *      .. deve-se listar todos os países disponíveis
+ *  . busca por data
+ * 
+ * - implementar verificação de disponibilidade da api primeiramente
+ */
 
 export default async () => {
-    let response = await window.fetch(apiUrl)
+    let response = await window.fetch(apiUrlBase)
     response = await response.json()
     return response.data
 }
