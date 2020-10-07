3c6866a13ce2293f97ddeea18049ba24066fa99f
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2020-10-07 21:39:13 +0000

    Add: functions to correctly render each table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import dictionary from "../resources/dictionary"
+import React from "react";
+import dictionary from "../resources/dictionary";
 
 function Headings({ dataHeadings }) {
-    return dataHeadings
-        .filter(filterDataWithDictionary)
-        .map((heading, key) =>
-            <div className="col" key={key}>
-                <h4>{dictionary[heading]}</h4>
-            </div>
-        )
+    return dataHeadings.filter(filterDataWithDictionary).map((heading, key) => (
+        <div className="col" key={key}>
+            <h4>{dictionary[heading]}</h4>
+        </div>
+    ));
 }
 
 function filterDataWithDictionary(heading) {
-    return dictionary.hasOwnProperty(heading)
+    return dictionary.hasOwnProperty(heading);
 }
 
 function parseHeadingsFromApiDataObject(apiDataObject) {
-    return Object.keys(apiDataObject)
+    return Object.keys(apiDataObject);
+}
+
+function renderTableBrazil(dataToRender) {
+    return (
+        <>
+            {renderInputBrazil(dataToRender)}
+            {dataToRender.map((dataReceived, key) => (
+                <div
+                    className="row data-rows align-items-center pb-3"
+                    key={key}
+                >
+                    {Object.keys(dataReceived).map((objectKey) =>
+                        filterDataWithDictionary(objectKey) ? (
+                            <div className="col align-middle">
+                                {dataReceived[objectKey]}
+                            </div>
+                        ) : undefined
+                    )}
+                </div>
+            ))}
+        </>
+    );
 }
 
 function renderInputBrazil(apiData) {
@@ -26,8 +46,10 @@ function renderInputBrazil(apiData) {
                 <label>Selecione o Estado</label>
                 <select>
                     {apiData.map((data, key) => (
-                        <option key={key} value={data.uf}>{data.state}</option>
-                        ))}
+                        <option key={key} value={data.uf}>
+                            {data.state}
+                        </option>
+                    ))}
                 </select>
             </div>
 
@@ -35,7 +57,7 @@ function renderInputBrazil(apiData) {
                 <input id="date" type="date" />
             </div>
         </div>
-    )
+    );
 }
 
 function renderInputWorld(apiData) {
@@ -44,36 +66,52 @@ function renderInputWorld(apiData) {
             <label>Selecione o País</label>
             <select>
                 {apiData.map((data, key) => (
-                    <option key={key} value={data.country}>{data.country}</option>
+                    <option key={key} value={data.country}>
+                        {data.country}
+                    </option>
                 ))}
             </select>
         </div>
-    )
+    );
+}
+
+function renderTableWorld(dataToRender) {
+    return (
+        <>
+            {renderInputWorld(dataToRender)}
+            {dataToRender.map((dataReceived, key) => (
+                <div
+                    className="row data-rows align-items-center pb-3"
+                    key={key}
+                >
+                    {Object.keys(dataReceived).map((objectKey) =>
+                        filterDataWithDictionary(objectKey) ? (
+                            <div className="col align-middle">
+                                {dataReceived[objectKey]}
+                            </div>
+                        ) : undefined
+                    )}
+                </div>
+            ))}
+        </>
+    );
 }
 
 export default function Table({ dataToRender }) {
-    return (<div>
-        <div className="grid">
-            <div className="row headings pb-2">
-                <Headings dataHeadings={parseHeadingsFromApiDataObject(dataToRender[0])} />
-            </div>
-            {dataToRender.map((dataReceived, key) =>
-                <div className="row data-rows align-items-center pb-3" key={key}>
-                    {Object.keys(dataReceived).map(objectKey => (
-                        filterDataWithDictionary(objectKey) ?
-                            <div className="col align-middle">
-                                {dataReceived[objectKey]}
-                            </div> :
-                            undefined
-                    ))}
+    return (
+        <div>
+            <div className="grid">
+                <div className="row headings pb-2">
+                    <Headings
+                        dataHeadings={parseHeadingsFromApiDataObject(
+                            dataToRender[0]
+                        )}
+                    />
                 </div>
-            )}
-            {/* {dataToRender.map((dataReceived, key) => (
-                <div className="row data-rows align-items-center pb-3" key={key}>
-                    {Object.keys(dataReceived).map(objectKey => (
-                        <div className="col align-middle">{dataReceived[objectKey]}</div>
-                    ))}
-                </div>))} */}
+                {dataToRender[0].uf
+                    ? renderTableBrazil(dataToRender)
+                    : renderTableWorld(dataToRender)}
+            </div>
         </div>
-    </div>)
+    );
 }
